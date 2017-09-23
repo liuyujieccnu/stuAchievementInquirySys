937a744f1a02192fcd3ea79f2a1674a4e59a85a1
liuyujieccnu <liuyujie@mails.ccnu.edu.cn>
2017-09-23 17:14:08 +0800

    完善添加、表格删除

--- js/main.js
+++ js/main.js
@@ -35,6 +35,29 @@ function verifyInputs(input) {
     return true;
 }
 
+function clazzAver(arr) {
+    let sum = 0;
+    for (let item of arr) {
+        sum += parseFloat(item.total);
+    }
+    let res = sum / arr.length;
+    return res.toFixed(2);
+}
+
+function clazzMedian(arr) {
+    let total = [];
+    let res = 0;
+    for (let item of arr) {
+        total.push(parseFloat(item.total));
+    }
+    total.sort((a, b) => a - b);
+    if (total.length % 2 === 0) {
+        res = (total[total.length / 2] + total[total.length / 2 - 1]) / 2;
+    } else {
+        res = total[Math.floor(total.length / 2)];
+    }
+    return res.toFixed(2);
+}
 
 function add() {
     let stuData = new Student($('#inputName').val(), $('#inputId').val(), $('#inputNational').val(),
@@ -59,35 +82,70 @@ function add() {
     window.location.reload();
 }
 
-function dataDisplay() {
+function dataDisplay(aver, median) {
     if (localStorage.getItem("students") === null) {
         // language=HTML
         $('#stuDataBody').append("<tr><td colspan='10' class='text-center' id ='zanwu'>暂无数据</td></tr>");
+        $('#Aver').after("<td colspan='2' class='text-center'>暂无数据</td>");
+        $('#Median').after("<td colspan='2' class='text-center'>暂无数据</td>");
     } else {
         $('#stuDataBody tr').remove();
         let students = JSON.parse(localStorage.getItem('students'));
-        students.map(function (obj) {
-            let tr = $('#stuDataBody').append(document.createElement('tr'));
-            tr.addClass('text-center');
-            for(let i in obj){
-                if(i !== 'national'){
+        students.map(function (obj, index) {
+            $('#stuDataBody').append("<tr></tr>").addClass('text-center');
+            let tr = $('#stuDataBody > tr')[index];
+            for (let i in obj) {
+                if (i !== 'national') {
                     let val = document.createElement('td');
                     val.innerText = obj[i];
                     tr.append(val);
                 }
             }
-            let del = document.createElement("td");
-            del.innerHTML = "<button class='btn btn-warning'>"+"删除"+"</button>";
-            tr.append(del);
+            let modify = document.createElement("td");
+            modify.innerHTML = "<button class='btn btn-warning tableButton'>" + "修改" + "</button>" + "<button class='btn btn-danger'>" + "删除" + "</button>";
+            tr.append(modify);
         });
+        $('#Aver').after("<td colspan='2' class='text-center'>" + aver + "</td>");
+        $('#Median').after("<td colspan='2' class='text-center'>" + median + "</td>");
+    }
+}
 
-
+function delData(tar) {
+    let students = JSON.parse(localStorage.getItem('students'));
+    let tarID = tar.parentElement.parentElement.children[1].innerText;
+    let message = confirm('警告：确定要删除,学号为' + tarID + '的同学的数据吗？');
+    if (message) {
+        localStorage.removeItem('students');
+        let delNo = 0;
+        students.forEach(function (obj, index) {
+            if (obj.id === tarID) {
+                delNo = index;
+            }
+        });
+        students.splice(delNo, 1);
+        if (students.length === 0) {
+            localStorage.clear();
+        } else {
+            localStorage.setItem("students", JSON.stringify(students));
+        }
+        window.location.reload();
     }
 }
 
 function main() {
-    dataDisplay();
+    let median = 0;
+    let aver = 0;
+    if (JSON.parse(localStorage.getItem('students'))) {
+        median = clazzMedian(JSON.parse(localStorage.getItem('students')));
+        aver = clazzAver(JSON.parse(localStorage.getItem('students')));
+    }
+    dataDisplay(aver, median);
     $('#storeStu').click(add);
+    $('#stuDataBody').on("click", function (event) {
+        if (event.target && event.target.nodeName === "BUTTON" && event.target.className === "btn btn-danger") {
+            delData(event.target);
+        }
+    });
 }
 
 main();
